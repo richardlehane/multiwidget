5a7be75def38d8aa757634e5d32f801c24269c9f
Richard Lehane <richard@itforarchivists.com>
2025-07-08 16:05:58 +1000

    Create multi_provider.dart

---
+++ lib/provider/multi_provider.dart
@@ -0,0 +1,36 @@
+// All on current node
+
+class Multi {
+  int len(String name) {
+    return 0;
+  }
+
+  // e.g. append("Disposal", "Disposal")
+  int append(String name, String el) { 
+    return 0;
+  }
+
+  // knows if el or attr
+  String get(String name, int idx, String tok) {
+    return "";
+  }
+
+  void set(String name, int idx, String tok, String val) {
+    return;
+  }
+
+  List<xml> getParagraphs(String name, int idx, String el) {
+
+  }
+
+  void setParagraphs(String name, int idx, String el, List<xml> val) {
+    return;
+  }
+
+  void moveUp(String name, int idx) {}
+  void moveDown(String name, int idx) {}
+  void drop(String name, int idx) {}
+  
+}
+
+
